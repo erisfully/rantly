0eea1e65c01ac5d9b8beba23280e39335cdb3e80
Ashley <a.clarkem@gmail.com>
2014-09-25 14:12:08 -0600

    user model spec tests for user creation

--- db/migrate/20140912175656_create_users.rb
+++ db/migrate/20140912175656_create_users.rb
@@ -1,6 +1,6 @@
-class CreateUsersTable < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users_tables do |t|
+    create_table :users do |t|
       t.string :username
       t.string :first_name
       t.string :last_name
