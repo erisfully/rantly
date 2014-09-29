192f699970c0f4ea6033d571a848226e7d14f10b
Ashley <a.clarkem@gmail.com>
2014-09-29 16:22:21 -0600

    user can register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :username, :first_name, :last_name, :password, :bio, :frequency, :presence => true
+  validates :username, :uniqueness => true
 end
