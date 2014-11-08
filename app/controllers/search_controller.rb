53dc619194ff78cc47463deb897847827a550a3f
Ashley <a.clarkem@gmail.com>
2014-11-07 17:03:31 -0700

    user can search by last name and user name

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController <  ApplicationController
   def index
     @rant = Rant.new
     @user = User.find(current_user.id)
-    @users_results = User.find_by_first_name(params[:search])
+    @users_results = User.find_by_first_name(params[:search]) || User.find_by_last_name(params[:search]) || User.find_by_username(params[:search])
   end
 
 end
