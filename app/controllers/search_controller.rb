dff670ad62f58f934078cbaa9b7592ebb185e348
Ashley <a.clarkem@gmail.com>
2014-11-10 14:36:19 -0700

    user can search rants and fixed user name search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
-class SearchController <  ApplicationController
+class SearchController < ApplicationController
   def index
     @rant = Rant.new
     @user = User.find(current_user.id)
-    @users_results = User.find_by_first_name(params[:search]) || User.find_by_last_name(params[:search]) || User.find_by_username(params[:search])
+    if params[:search]
+      @search_results = Rant.search(params[:search])
+    end
+    @follow = Follow.all
+    @favorite = Favorite.all
   end
 
 end
--- app/models/rant.rb
+++ app/models/rant.rb
@@ -9,4 +9,13 @@ class Rant < ActiveRecord::Base
   def self.mentions(user)
     Rant.where('rant LIKE ?', '%@' + user.username + '%')
   end
+
+  def self.search(search)
+    joins(:user).where('users.username = ? OR
+      users.first_name = ? OR
+      users.last_name = ? OR
+      rants.rant LIKE ? OR
+      rants.topic LIKE ?',
+      search, search, search, '%' + search + '%', '%' + search + '%')
+  end
 end
