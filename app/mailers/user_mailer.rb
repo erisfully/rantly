9cec89f55abc86049ffb921d8aeb81c5c792556b
Ashley <a.clarkem@gmail.com>
2014-11-17 18:46:24 -0700

    even more fun with mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
   default :from => 'admin@rantlyapp.com'
-  default :host => Rails.root
+  default host: Rails.root
 
   def registration_email(user)
     @url = Rails.env.production? ? 'http://rantlyapp.herokuapp.com/' : 'http://localhost:3000'
-    mail(:to => user.email, :subject => "Registered!", :subject => "Thanks for registering!")
+    mail(:to => user.email, :subject => "Thanks for registering!")
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -43,16 +43,6 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-  
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
 
   config.action_mailer.perform_deliveries  = true
   config.action_mailer.default_url_options = { :host => 'rantlyapp.herokuapp.com' }
