2b4c11502383b861066882eb7fea2ad96bee2bee
Ashley <a.clarkem@gmail.com>
2014-11-17 18:10:02 -0700

    more mail fixes;

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,9 @@
 class UserMailer < ActionMailer::Base
   default :from => 'admin@rantlyapp.com'
+  default :host => Rails.root
 
   def registration_email(user)
+    @url = Rails.env.production? ? 'http://rantlyapp.herokuapp.com/' : 'http://localhost:3000'
     mail(:to => user.email, :subject => "Registered!", :subject => "Thanks for registering!")
   end
 end
